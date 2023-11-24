0d1a565fc09e9ca1cd3942ec7509f7b6a6a9f239
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-11-24 23:20:56 +0000

    Deploying to gh-pages from @ Makorishko/goit-react-hw-05-movies@5bab2c43287ba164fb4ea86487e197eca83dfcd7 🚀

---
+++ static/js/520.6f303755.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(87),o=r(184);function a(){return(0,o.jsxs)("div",{children:["Not found page, please go to",(0,o.jsx)(n.rU,{to:"/",children:" Homepage"})]})}}}]);
+//# sourceMappingURL=520.6f303755.chunk.js.map
